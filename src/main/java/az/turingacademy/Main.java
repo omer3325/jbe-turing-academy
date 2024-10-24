7c5c18d17f4f6283d3a87b8a09c5f669446eeeff
omer3325 <omerelekberov22@gmail.com>
2024-10-24 21:18:48 +0400

    commit

---
+++ src/main/java/az/turingacademy/Main.java
@@ -0,0 +1,14 @@
+package az.turingacademy;
+
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.print("Hello World!");
+        System.out.println("Hello , World");
+        System.out.printf("Hello %s"+"World");
+        System.out.printf("Hello World : %.2f",98.747); */
+
+        System.out.println("1\n2\n3\n4\n5");
+
+
+    }
+}
