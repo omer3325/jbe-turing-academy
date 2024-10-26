b033f44c91d3347f75c44e75b5be7f2ebd8611ca
omer3325 <omerelekberov22@gmail.com>
2024-10-26 15:54:18 +0400

    pirimitive type

--- src/main/java/az/turingacademy/Main.java
+++ src/main/java/az/turingacademy/Main.java
@@ -7,7 +7,29 @@ public class Main {
         System.out.printf("Hello %s"+"World");
         System.out.printf("Hello World : %.2f",98.747); */
 
-        System.out.println("1\n2\n3\n4\n5");
+        //System.out.println("1\n2\n3\n4\n5");
+        byte deyer1 = 5;
+        short deyer2 = 6;
+        int deyer3 = 7;
+        long deyer4 = 15;
+        System.out.println("tam ededler = "+(deyer1+deyer2+deyer3+deyer4));
+
+        double deyer5 = 12.5;
+        double deyer6 = 10.9f;
+
+        System.out.println("kesir ededler ="+(deyer5+deyer6));
+
+        char simvol = 'T';
+
+        System.out.println("simvol ="+simvol);
+        int t = 10;
+        boolean muqayise = t>15;
+
+        System.out.println("muqayise ="+muqayise);
+
+
+
+
 
 
     }
